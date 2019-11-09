867d9e63fb8b243089f950d2e85827812c763f1b
Markus <ghsthack@gmail.com>
2019-11-09 07:53:44 +0100

    Convert a whole folder at a time
    + transparency fix.

--- frm2png/Program.cs
+++ frm2png/Program.cs
@@ -16,12 +16,25 @@ namespace frm2png
                 return;
             }
 
+            string dst = null;
+
+            if (args.Length == 2)
+                dst = args[1];
+
+
+            if (Directory.Exists(args[0]))
+            {
+                foreach(var c in Directory.GetFiles(args[0]))
+                {
+                    if (Path.GetExtension(c.ToLower()) == ".frm")
+                        Convert(c, dst);
+                }
+                Environment.Exit(0);
+            }
+
             if (!File.Exists(args[0]))
                 Console.WriteLine($"{args[0]} is not a valid file.");
-            if(args.Length == 1)
-                Convert(args[0], null);
-            else 
-                Convert(args[0], args[1]);
+            Convert(args[0], dst);
         }
 
         static void Convert(string input, string outputDir)
@@ -29,8 +42,10 @@ namespace frm2png
             if (outputDir == null)
                 outputDir = Path.GetDirectoryName(input);
 
+
             var bmp = FalloutFRMLoader.Load(File.ReadAllBytes(input));
             var c = new Bitmap(bmp[0]);
+            c.MakeTransparent(Color.FromArgb(11, 0, 11));
             var filename = Path.GetFileNameWithoutExtension(input);
             var outpath = outputDir + "\\" + filename + ".png";
             Console.WriteLine($"Saving file to {outpath}");
